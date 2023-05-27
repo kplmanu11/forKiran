0c94c91fa1af0e89515965ee47ced0c8c65252e9
Kapil Neupane <kpl.neupane11@gmail.com>
2023-05-27 22:01:54 +0545

    code added by kapil

---
+++ src/FirstAssignment/byKapil.java
@@ -0,0 +1,10 @@
+package FirstAssignment;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("created by kapil);
+	}
+
+}
